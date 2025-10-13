ce70df33ca50b0ef139a8aece0ca02566ecc0771
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2025-10-13 11:46:39 -0500

    Add rutes

--- emissions_project/urls.py
+++ emissions_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from emissions_app.infrastructure.views import EmissionListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/emissions/', EmissionListView.as_view(), name='emission-list'),
 ]
